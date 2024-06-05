f437dc346746bdf8d6a4c3e96cdf4dc270f1198b
Moka <yisida70@gmail.com>
2024-06-05 15:02:32 +0900

    Organize files

--- vue_eaglesS2/src/App.vue
+++ vue_eaglesS2/src/App.vue
@@ -1,85 +1,34 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup></script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <body>
+    <div class="back_Box_All">
+      <div class="back_Box">
+        <div class="flicker">
+          <a id="example" href="./main.html">주문하러 가기</a>
+        </div>
+      </div>
     </div>
-  </header>
-
+    <div id="btnfadeIn">
+      <div>
+        <iframe
+          src="/video/KFC_CF.mp4"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        >
+        </iframe>
+      </div>
+      <div @click="skip">
+        <div class="skip_Box">
+          <p>skip</p>
+        </div>
+      </div>
+    </div>
+  </body>
   <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
--- vue_eaglesS2/src/main.js
+++ vue_eaglesS2/src/main.js
@@ -1,11 +1,11 @@
-import './assets/main.css'
-
+import './assets/css/Lee-css.css'
+import './assets/css/reset.css'
+import './assets/js/index.js'
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 
 const app = createApp(App)
 
-app.use(router)
+
 
 app.mount('#app')
