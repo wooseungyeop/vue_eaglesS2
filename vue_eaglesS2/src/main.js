fee44e8e881ba55b258072deb5093e34ba3fdad8
Moka <yisida60@gmail.com>
2024-06-10 11:03:38 +0900

    Del-js

--- vue_eaglesS2/src/main.js
+++ vue_eaglesS2/src/main.js
@@ -2,7 +2,6 @@ import "./assets/css/Lee-css.css";
 import "./assets/css/reset.css";
 import "./assets/css/font.css";
 import "./assets/css/index.css";
-import "./assets/js/video.js";
 import "./assets/css/mainmenu-popup.css";
 import "./assets/css/payment.css";
 import "./assets/css/global.css";
