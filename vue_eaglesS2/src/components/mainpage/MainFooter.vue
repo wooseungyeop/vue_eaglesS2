81c4f107f1d7d993b581b463d8c9ecdff2637e2d
dansun2 <dansunmusik7@gmail.com>
2024-06-10 17:33:50 +0900

    add_lang_button

--- vue_eaglesS2/src/components/mainpage/MainFooter.vue
+++ vue_eaglesS2/src/components/mainpage/MainFooter.vue
@@ -1,21 +1,29 @@
+<script setup>
+import { NotImplemented } from "@/assets/js/mainMenu_js/modal";
+
+const showNotImplemented = () => {
+  NotImplemented();
+};
+</script>
+
 <template>
     <div class="footerBox">
-      <div class="languageBox" @click="lang">Language</div>
+      <div class="languageBox" @click="showNotImplemented">Language</div>
       <div class="guideBox">
         <p>현금 결제는 카운터에서 가능합니다.</p>
       </div>
     </div>
 </template>
 
-<script setup>
-    // import { useRoute } from 'vue-router';
-    // const route = useRoute();
+<!-- <script setup>
+    import { useRoute } from 'vue-router';
+    const route = useRoute();
      
-    // function lang() {
-    //     alert("언어선택");
-    //     route.push("/language")
-    // }
-</script>
+    function lang() {
+         alert("언어선택");
+         route.push("/language")
+    }
+</script> -->
 
 <style scoped>
 </style>
